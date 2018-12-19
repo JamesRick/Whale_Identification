563439e7764b1c7f4990d64b9b1c14e6ca2fd327
JamesRick <james.rick.1995@gmail.com>
2018-12-19 18:35:52 -0500

    adding initial files ;

---
+++ src/class_counter.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def count_classes():
+    pass
+
+if __name__ == '__main__':
+    count_classes()
---
+++ src/siamese_cnn.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pdb
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+    # Input data
+    tf_train_data
+    tf_train_labels
+
+    tf_valid_data
+    tf_valid_labels
+
+    tf_test_data
+    tf_test_labels
