b38e3a0ba92d229b74ae9b3e4a050a7913bf115c
James Rick <jrick6@gatech.edu>
2018-12-19 19:27:04 -0500

    beginning code of siamese cnn

--- src/siamese_cnn.py
+++ src/siamese_cnn.py
@@ -8,11 +8,22 @@ graph = tf.Graph()
 with graph.as_default():
 
     # Input data
-    tf_train_data
-    tf_train_labels
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
 
-    tf_valid_data
-    tf_valid_labels
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
 
-    tf_test_data
-    tf_test_labels
+    # Model
+    def model(data):
+        conv1 = tf.layers.conv2d(
+            inputs=data,
+            filters=64,
+            kernel_size=[10,10]
+            padding="same",
+            activation=tf.nn.relu)
+    # Training Computation
+
+    # Optimizer
+    optimizer = tf.train.Momentum(learning_rate=0.5, momentum=0.01).minimize(loss)
+    # Predictions
